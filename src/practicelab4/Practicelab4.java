a5f6c8b3f11db3774d6bd10fc99d6efd023dd270
PaulinaEEF <paulinaeuceda@gmail.com>
2020-02-13 19:36:04 -0600

    mensaje

--- src/practicelab4/Practicelab4.java
+++ src/practicelab4/Practicelab4.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Practicelab4 {
 
     public static void main(String[] args) {
-       
+        System.out.println("Si me dio");
+        System.out.println("Sos un odioso, Yuda");
     }
     
 }
